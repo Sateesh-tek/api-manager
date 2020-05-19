9dac560fc66969e046c160104fc09211619d721d
Sateesh-tek <64182798+Sateesh-tek@users.noreply.github.com>
2020-05-19 16:18:29 +0530

    Update api.sh

--- api.sh
+++ api.sh
@@ -8,7 +8,7 @@ echo "API_ID" $Target_Environment
 if [ $Actions == "Create" ]
 then
    
-   anypoint-cli --username="$USR" --password="$PSW" --organization="$Business_Group" --environment="$Target_Environment" api-mgr api manage --type raml $API_Asset_Id $API_Asset_Version --muleVersion4OrAbove true >> file.txt
+   anypoint-cli --username=$USR --password=$PSW --organization=$Business_Group --environment=$Target_Environment api-mgr api manage --type raml $API_Asset_Id $API_Asset_Version --muleVersion4OrAbove true >> file.txt
     
  awk '/new/  {print $NF}' file.txt >> file2.txt
 
@@ -16,17 +16,17 @@ then
  
  echo "API_ID" $API_ID
 
-   anypoint-cli --username="$USR" --password="$PSW" --organization="$Business_Group" --environment="$Target_Environment" api-mgr policy apply -c '{"credentialsOriginHasHttpBasicAuthenticationHeader":"customExpression","clientIdExpression":"#[attributes.headers[\"client_id\"]]","clientSecretExpression":"#[attributes.headers[\"client_secret\"]]"}' $API_ID client-id-enforcement --groupId 68ef9520-24e9-4cf2-b2f5-620025690913 --policyVersion 1.2.1
+   anypoint-cli --username=$USR --password=$PSW --organization=$Business_Group --environment=$Target_Environment api-mgr policy apply -c '{"credentialsOriginHasHttpBasicAuthenticationHeader":"customExpression","clientIdExpression":"#[attributes.headers[\"client_id\"]]","clientSecretExpression":"#[attributes.headers[\"client_secret\"]]"}' $API_ID client-id-enforcement --groupId 68ef9520-24e9-4cf2-b2f5-620025690913 --policyVersion 1.2.1
   
 
 elif [  $Actions == "Update" ]
 then
 
-   anypoint-cli --username="$anypoint_usr" --password="$anypoint_pws" --organization="$Business_Group" --environment="$Target_Environment" api-mgr api change-specification -o json $API_InstanceId $API_Asset_Version
+   anypoint-cli --username=$USR --password=$PSW --organization=$Business_Group --environment=$Target_Environment api-mgr api change-specification -o json $API_InstanceId $API_Asset_Version
    
 elif [ $Actions == "Promote" ]
 then
-   anypoint-cli --username="$anypoint_usr" --password="$anypoint_pws" --organization="$Business_Group" --environment="$Target_Environment" api-mgr api promote -o json --copyPolicies true $API_InstanceId "da669ba5-e2b1-41ff-9bff-f1350361776d"
+   anypoint-cli --username=$USR --password=$PSW --organization="$Business_Group" --environment=$Target_Environment api-mgr api promote -o json --copyPolicies true $API_InstanceId "da669ba5-e2b1-41ff-9bff-f1350361776d"
 else
    echo "None of the condition met"
 fi
