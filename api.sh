245e4359b89330a84bbaf5395bf337aadae4ffa4
Sateesh-tek <64182798+Sateesh-tek@users.noreply.github.com>
2020-05-28 15:22:06 +0530

    Update api.sh

--- api.sh
+++ api.sh
@@ -5,7 +5,7 @@ echo "API_ID" $PSW
 echo "API_ID" $Target_Environment
 echo "API_ID"  ${Actions}
 
-if [ $Actions == "policy" ]
+if [ ${Actions} == "policy" ]
 then
    echo "policy12346" 
     
@@ -17,9 +17,9 @@ then
 
    anypoint-cli --username=$USR --password=$PSW --organization=$Business_Group --environment=$Target_Environment api-mgr policy apply -c '{"credentialsOriginHasHttpBasicAuthenticationHeader":"customExpression","clientIdExpression":"#[attributes.headers[\"client_id\"]]","clientSecretExpression":"#[attributes.headers[\"client_secret\"]]"}' $API_ID client-id-enforcement --groupId 68ef9520-24e9-4cf2-b2f5-620025690913 --policyVersion 1.2.1
 
-elif [ $Actions == "Create" ]
+elif [ ${Actions} == "Create" ]
 then
-   echo "Create"
+   echo "Create123"
    anypoint-cli --username=$USR --password=$PSW --organization=$Business_Group --environment=$Target_Environment api-mgr api manage --type raml $API_Asset_Id $API_Asset_Version --muleVersion4OrAbove true >> file.txt 
 
 elif [  $Actions == "Update" ]
